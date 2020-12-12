e41e2a4dd9ac165420e2a8ef30a21bdb679d58ed
Qing Ma <gsmofgsm@hotmail.com>
2020-12-12 13:00:57 +0100

    check subsription status

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,20 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/subscribe', function () {
+Route::middleware(['auth:sanctum', 'verified', 'nonPayingCustomer'])->get('/subscribe', function () {
     return view('subscribe', [
         'intent' => auth()->user()->createSetupIntent()   // https://laravel.com/docs/8.x/billing#payment-methods-for-subscriptions
     ]);
 })->name('subscribe');
 
-Route::middleware(['auth:sanctum', 'verified'])->post('/subscribe', function (Request $request) {
+Route::middleware(['auth:sanctum', 'verified', 'nonPayingCustomer'])->post('/subscribe', function (Request $request) {
 //    dd($request->all());
     auth()->user()->newSubscription(
         'cashier', $request->plan
     )->create($request->paymentMethod);
     return redirect(route('dashboard'));
 })->name('subscribe.post');
+
+Route::middleware(['auth:sanctum', 'verified', 'payingCustomer'])->get('/members', function () {
+    return view('members');
+})->name('members');
