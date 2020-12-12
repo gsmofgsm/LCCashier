eb9dbff6970d37c9f9560de7d76938981e1bc650
Qing Ma <gsmofgsm@hotmail.com>
2020-12-12 23:41:59 +0100

    install mollie

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,11 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
+            $table->string('mollie_customer_id')->nullable();
+            $table->string('mollie_mandate_id')->nullable();
+            $table->decimal('tax_percentage', 6, 4)->default(0); // optional
+            $table->dateTime('trial_ends_at')->nullable(); // optional
+            $table->text('extra_billing_information')->nullable(); // optional
             $table->timestamps();
         });
     }
